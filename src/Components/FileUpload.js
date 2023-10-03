c1ded45c82d27fd307002413d7401984433a121f
Ankit Kadyan <ankit.kadyan@designx.in>
2023-10-03 16:03:14 +0530

    close button errors resolved

--- src/Components/FileUpload.js
+++ src/Components/FileUpload.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
-// import Button from 'react-bootstrap/Button';
+import Close from "./assets/close.svg";
 
 const FileUpload = () => {
   const [fileName, setFileName] = useState([]);
@@ -10,22 +10,25 @@ const FileUpload = () => {
   const handleFileChange = (e) => {
     const file = e.target.files[0];
     if (file) {
-      setFileName((prevFileName) => [...prevFileName, file.name]);
+      const updatedFileNames = [...fileName, file.name];
+      setFileName(updatedFileNames);
 
-      localStorage.setItem('uploadedFiles', JSON.stringify([...fileName, file.name]));
+      localStorage.setItem("uploadedFiles", JSON.stringify(updatedFileNames));
     }
   };
+
+  const handleClose = (index) => {
+    const updatedFileNames = fileName.filter((_, i) => i !== index);
+    setFileName(updatedFileNames);
+    localStorage.setItem("uploadedFiles", JSON.stringify(updatedFileNames));
+  };
+
   useEffect(() => {
-    
-    const savedFiles = localStorage.getItem('uploadedFiles');
+    const savedFiles = localStorage.getItem("uploadedFiles");
     if (savedFiles) {
       setFileName(JSON.parse(savedFiles));
     }
   }, []);
-//   const handleClose = () => {
-//     localStorage.removeItem('uploadedFiles');
-//     setFileName(false);
-//   };
 
   return (
     <div className="text-cyan-950 w-[100%] pt-[20px] mt-[40px]">
@@ -53,21 +56,37 @@ const FileUpload = () => {
       ) : fileName.length !== 0 ? (
         <div className="mt-[20px] mr-[20px] w-[50%] ml-[25%] block lg:grid lg:grid-cols-3 gap-4 font-poppins">
           {fileName.map((fileName, index) => (
-            <Link to={`/chat/1?fileNames=${fileName}`} className="no-underline" key={index}>
-              <CardGroup>
-                <Card style={{ width: "100%" }} className="shadow-md mb-3 lg:mb-0">
-                  <Card.Body>
-                    <Card.Title className="text-lg h-12 mb-5" style={{ color: "#3b82f6" }}>
+            <CardGroup>
+              <Card
+                style={{ width: "100%" }}
+                className="shadow-md mb-3 lg:mb-0"
+              >
+                <Card.Body>
+                  <Card.Title
+                    className="flex justify-between text-lg h-12 mb-5"
+                    style={{ color: "#3b82f6" }}
+                  >
+                    <Link
+                      to={`/chat/1?fileNames=${fileName}`}
+                      className="no-underline"
+                      key={index}
+                    >
                       {fileName}
-                    </Card.Title>
-                    <Card.Text>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, nisi?
-                    </Card.Text>
-                    {/* <Button onChange={handleClose} variant="danger">Close</Button> */}
-                  </Card.Body>
-                </Card>
-              </CardGroup>
-            </Link>
+                    </Link>
+                    <img
+                      src={Close}
+                      onClick={()=>{handleClose(index)}}
+                      alt=""
+                      className="w-[10%] mb-[20px] cursor-pointer"
+                    />
+                  </Card.Title>
+                  <Card.Text>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Veritatis, nisi?
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </CardGroup>
           ))}
         </div>
       ) : null}
